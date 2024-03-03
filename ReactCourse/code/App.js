83862a31df4033188c44cb4d5b80200c61515610
whitebumblebee <shishirjhaccr@gmail.com>
2024-03-04 01:06:55 +0530

    Add changes for the initial foundation

--- ReactCourse/code/App.js
+++ ReactCourse/code/App.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const heading = React.createElement('h1',
+    {id: 'heading'},
+    'Shishir is here from React create Element'
+)
+const jsxHeading = <h1 id='heading'>Shishir is here from React create Element</h1>
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(jsxHeading)
